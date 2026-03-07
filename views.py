619e77719f438ab63ef5e5faf578b6524460311e
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-03-07 13:18:23 +0100

    feat: convert drawer panels to full-page forms

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ Projects & Time Tracking Module Views
 """
 from django.core.paginator import Paginator
 from django.db.models import Q, Count
+from django.http import HttpResponse
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render as django_render
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -113,6 +115,7 @@ def projects_list(request):
     }
 
 @login_required
+@htmx_view('projects/pages/project_add.html', 'projects/partials/project_add_content.html')
 def project_add(request):
     hub_id = request.session.get('hub_id')
     if request.method == 'POST':
@@ -134,10 +137,13 @@ def project_add(request):
         obj.budget = budget
         obj.spent = spent
         obj.save()
-        return _render_projects_list(request, hub_id)
-    return django_render(request, 'projects/partials/panel_project_add.html', {})
+        response = HttpResponse(status=204)
+        response['HX-Redirect'] = reverse('projects:projects_list')
+        return response
+    return {}
 
 @login_required
+@htmx_view('projects/pages/project_edit.html', 'projects/partials/project_edit_content.html')
 def project_edit(request, pk):
     hub_id = request.session.get('hub_id')
     obj = get_object_or_404(Project, pk=pk, hub_id=hub_id, is_deleted=False)
@@ -152,7 +158,7 @@ def project_edit(request, pk):
         obj.spent = request.POST.get('spent', '0') or '0'
         obj.save()
         return _render_projects_list(request, hub_id)
-    return django_render(request, 'projects/partials/panel_project_edit.html', {'obj': obj})
+    return {'obj': obj}
 
 @login_required
 @require_POST
